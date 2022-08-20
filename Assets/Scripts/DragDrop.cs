36ca6d45b31695f0f0b5c5b89eaf7edb6b3b8670
Zihao2314435 <zihaolin829@gmail.com>
2022-08-20 17:22:41 -0400

    Zihao Commit DragDrop

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    public Canvas canvas;
+    private RectTransform rectTransform;
+
+    private void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+
+    }
+
+
+
+    public void OnPointerDown(PointerEventData evenData) {
+        Debug.Log("OnPointerDown");
+    }
+  
+  
+    public void OnEndDrag(PointerEventData eventData) {
+        Debug.Log("OnEndDrag");
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        Debug.Log("OnBeginDrag");
+    }
+
+    
+    public void OnDrag(PointerEventData eventData) {
+        Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+
+}
+
