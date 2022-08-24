40a71d89fe5735971f1e990e0277880b8147db64
Zihao2314435 <zihaolin829@gmail.com>
2022-08-23 20:28:41 -0400

    In game UI update

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -6,11 +6,23 @@ using UnityEngine.EventSystems;
 public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     public Canvas canvas;
+
     private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    private GameObject clone;
+
+    private bool inSlot = false;
 
     private void Awake() {
         rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        clone = gameObject; 
 
+    }
+
+    public void slotted()
+    {
+        inSlot = true; 
     }
 
 
@@ -22,15 +34,26 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
   
     public void OnEndDrag(PointerEventData eventData) {
         Debug.Log("OnEndDrag");
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f;
+        if(!inSlot)
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void OnBeginDrag(PointerEventData eventData) {
         Debug.Log("OnBeginDrag");
+        GameObject newClone = Instantiate(clone, transform.position, transform.rotation, canvas.transform);
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = .6f;
+        inSlot = false;
+       
     }
 
     
     public void OnDrag(PointerEventData eventData) {
-        Debug.Log("OnDrag");
+        //Debug.Log("OnDrag");
         rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
     }
 
