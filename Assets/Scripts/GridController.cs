c70ff57447e0f1eb5f430d126687ab8c29660707
Zihao2314435 <zihaolin829@gmail.com>
2022-10-08 16:06:33 -0400

    testing

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    public GameObject player; 
+   
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
