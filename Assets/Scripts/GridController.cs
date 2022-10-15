e13543ff8c70a56635b5064af647b28d8fdc45b6
Cameron Ghaemmaghami <cghaemm@yahoo.com>
2022-10-15 13:02:28 -0700

    InspectorGrid Done; Moving on to Grid Controller Script

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -6,17 +6,54 @@ namespace Array2DEditor
 {
     public class GridController : MonoBehaviour
     {
+
+        // 0 is an empty space
+        // 1 is player car
+        // 2 is npc_car
+        // 3 is goal
+        // 4 is sports_car;
+
         public GameObject player;
         public GameObject npc_car;
+        public GameObject goal;
+        public GameObject sports_car;
 
         [SerializeField]
-        private Array2DInt test = null;
+        private Array2DInt grid = null;
 
         // Start is called before the first frame update
         void Start()
         {
-            // numColumns = grid[0].GetLength();
-            // numRows = grid.GetLength();
+            int numRows = grid.GridSize.y;
+            int numColumns = grid.GridSize.x;
+            Debug.Log("Number of Rows: " + numRows);
+            Debug.Log("Number of Columns: " + numColumns);
+
+            float width = gameObject.transform.localScale.z;
+            float length = gameObject.transform.localScale.x;
+            Debug.Log("Width of Road: " + width);
+            Debug.Log("Length of Road: " + length);
+
+            int[,] newGrid = grid.GetCells();
+            // newGrid[y, x]
+
+            for(int y = 0; y < numRows; y++)
+            {
+                for(int x = 0; x < numColumns; x++)
+                {
+                    Debug.Log("[" + y + ", " + x + "]: " + newGrid[y, x]);
+                    if(newGrid[y,x] == 2)
+                    {
+                        Vector3 position = new Vector3(gameObject.transform.position.x + ((width/numColumns)*x), 
+                        gameObject.transform.position.y, gameObject.transform.position.z + ((length/numRows)*y));
+                        
+                        GameObject car = Instantiate(npc_car, position, transform.rotation); 
+
+                    }
+
+                }
+            }
+
             
         }
 
